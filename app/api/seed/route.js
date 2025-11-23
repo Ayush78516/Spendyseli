84e2f47aa284276f8e9cac25b763fe40ab795ace
Ayush Pandey <ay7113pandey@gmail.com>
2025-11-23 23:54:45 +0530

    Fix middleware and add Arcjet to seed route

--- app/api/seed/route.js
+++ app/api/seed/route.js
@@ -1,6 +1,27 @@
+// import { seedTransactions } from "@/actions/seed";
+
+// export async function GET(){
+//     const result=await seedTransactions();
+//     return Response.json(result);
+// }
+import arcjet, { shield, detectBot } from "@arcjet/next";
 import { seedTransactions } from "@/actions/seed";
 
-export async function GET(){
-    const result=await seedTransactions();
-    return Response.json(result);
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+
+export async function GET(req) {
+  // Arcjet protection
+  await aj.protect(req);
+
+  const result = await seedTransactions();
+  return Response.json(result);
 }
